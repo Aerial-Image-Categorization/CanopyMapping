6f0c95af13db4cc3e9f237fbb6aade70b048e60a
mbnczy <banoczy.martin@gmail.com>
2024-04-13 16:35:06 +0000

    new folder structure update

--- utils/datasets.py
+++ utils/datasets.py
@@ -103,14 +103,17 @@ class ImageDataset(Dataset):
     
 class ImageNameDataset(Dataset):
     def __init__(self, dataset_path, sort=False) -> None:
-        files = os.listdir(dataset_path)
+        image_filenames = os.listdir(os.path.join(dataset_path,'images'))
+        mask_filenames = os.listdir(os.path.join(dataset_path,'masks'))
         self.images_path = []
         self.masks_path = []
-        for file in files:
+        for file in image_filenames:
             if file.split('_')[1]=='tif':
-                self.images_path.append(os.path.join(dataset_path,file))
-                self.masks_path.append(os.path.join(dataset_path,file.replace('tif','shp')))
-        if True:
+                self.images_path.append(os.path.join(os.path.join(dataset_path,'images'),file))
+        for file in mask_filenames:
+            if file.split('_')[1]=='shp':
+                self.masks_path.append(os.path.join(os.path.join(dataset_path,'masks'),file))
+        if sort:
             self.images_path = sorted(self.images_path, key=lambda x: (int(os.path.basename(x).split('_')[2]), int(os.path.basename(x).split('.')[0].split('_')[3])))
             self.masks_path = sorted(self.masks_path, key=lambda x: (int(os.path.basename(x).split('_')[2]), int(os.path.basename(x).split('.')[0].split('_')[3])))
 
