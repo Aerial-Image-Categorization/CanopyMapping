f91ca892b308361b9c3292896a72f6c088746977
mbnczy <banoczy.martin@gmail.com>
2024-06-16 17:23:49 +0000

    middle split fixes

--- utils/traintestsplit.py
+++ utils/traintestsplit.py
@@ -1,27 +1,40 @@
 import os
+import math
 
 def middle_split(images, masks, train_size):
-    test_images_count = int(len(images)*(1-train_size))
+    test_images_count = int(len(images)*round(1-train_size,5))
     X = Y = math.sqrt(test_images_count)
-    if type(Y)==float:
-        X = int(X)
-        Y = test_images_count // int(Y)
-
     max_x, max_y = os.path.splitext(os.path.basename(images[-1]))[0].split('_')[2:4]
-    mid_x = int(int(max_x)/2)
-    mid_y = int(int(max_y)/2)
-    x_start = mid_x//2 - X//2
-    y_start = mid_y//2 - Y//2
+    print(test_images_count, X)
+    if type(Y)==float:
+        if int(max_x)>int(max_y):
+            X = int(X)
+            Y = test_images_count // int(X)
+        elif int(max_x)<int(max_y):
+            Y = int(X)
+            X = test_images_count // int(X)
+        else:
+            X = int(X)
+            Y = test_images_count // int(X)
+    print('xy',X,Y)
+    print('max',max_x,max_y)
+    mid_x = int(max_x)//2
+    mid_y = int(max_y)//2
+    print('mid',mid_x,mid_y)
+    x_start = mid_x - X//2
+    y_start = mid_y - Y//2
 
     images_test=[]
     masks_test=[]
 
     x_end = x_start+X
     y_end = y_start+Y
-    for x in range(x_start,x_end+1):
-        for y in range(y_start,y_end+1):
-            image_name = images[0][:-17]+f'tile_tif_{x}_{y}.png'
-            mask_name = images[0][:-24]+f'masks/tile_shp_{x}_{y}.png'
+    print(x_start,x_end)
+    print(y_start,y_end)
+    for x in range(x_start,x_end):
+        for y in range(y_start,y_end):
+            image_name = os.path.join(images[0][:-17],f'tile_tif_{x}_{y}.png')
+            mask_name = os.path.join(images[0][:-24],f'masks/tile_shp_{x}_{y}.png')
             if image_name in images:
                 images.remove(image_name)
                 images_test.append(image_name)
