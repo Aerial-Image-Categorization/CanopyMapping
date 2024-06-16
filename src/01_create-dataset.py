a9d7ef996cd24656c7ebefb373eba4b1e69ae4f6
mbnczy <banoczy.martin@gmail.com>
2024-06-16 18:42:19 +0000

    train-val-test split

--- src/01_create-dataset.py
+++ src/01_create-dataset.py
@@ -9,7 +9,11 @@ from utils.datasetvalidation import set_valid_CRS
 
 from utils.dataloading import ImageNameDataset
 from utils.traintestsplit import middle_split
+import torch
 from torch.utils.data import DataLoader, random_split
+
+import shutil
+import logging
 
 '''
 pipeline:
@@ -17,6 +21,7 @@ pipeline:
     - set_CRS
     - createPNG_Dataset
     - train-test split
+    - 
     - train-validation split
     - drop outlier pairs from any set
     - drop empty pairs from train & validation set
@@ -24,29 +29,61 @@ pipeline:
 '''
 
 if __name__ == '__main__':
-    folder = '../data/2024-05-20-dataset/standard/original'
-    out_folder = '../data/2024-05-20-dataset/standard/formatted'
-    size = (200,200)
-
-    #os.makedirs(folder, exist_ok=True)
-    #os.makedirs(out_folder, exist_ok=True)
-
-    ##split(
-    #    '../data/all_data/2023-02-23_Bakonyszucs_actual.tif',
-    #    '../data/all_data/Fa_pontok.shp',
-    #    folder,
-    #    size
-    #)
-
-    #set_valid_CRS(os.path.join(folder,'shps'), desired_crs_epsg=23700)
-    #createPNG_Dataset(folder,out_folder,size, point_size=28, grayscale=False)
+    #set logging
+    logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename='./logs/create-dataset.log',
+                    filemode='w')
     
-
-    dataset_folder = '../data/2024-04-21-dataset/standard/formatted'
-    train_size = 0.90
+    dataset_folder = '../data/2024-06-14-dataset'
+    size = (200,200)
+    tif_path = '../data/test_data/orig_test2.tif'
+    shp_path = '../data/all_data/Fa_pontok.shp'#'../data/test_data/conc_biomed_full.shp'
+    train_size = 0.8
+    valid_size = 0.1
     batch_size = 1
 
-    dataset = ImageNameDataset(dataset_folder, sort = True)
+    os.makedirs(os.path.join(dataset_folder, 'train','images'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'train','masks'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'val','images'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'val','masks'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'test','images'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'test','masks'), exist_ok=True)
+    
+    os.makedirs(os.path.join(dataset_folder,'standard','original'),exist_ok=True) #folder
+    os.makedirs(os.path.join(dataset_folder,'standard','formatted'),exist_ok=True) #out_folder
+
+    #split into tiles
+    split(
+        tif_path,
+        shp_path,
+        os.path.join(dataset_folder,'standard','original'),
+        size
+    )
+    
+    #set epsg
+    set_valid_CRS(
+        os.path.join(dataset_folder,'standard','original','shps'),
+        desired_crs_epsg=23700
+    )
+    
+    #format
+    if createPNG_Dataset(
+        os.path.join(dataset_folder,'standard','original'),
+        os.path.join(dataset_folder,'standard','formatted'),
+        size,
+        point_size=28,
+        grayscale=False
+    ) == False:
+        raise Exception('ERROR: check the logs')
+
+    #train - test
+    dataset = ImageNameDataset(
+        os.path.join(dataset_folder,'standard','formatted'),
+        sort = True
+    )
+    
+    print(len([sample['image'] for sample in dataset]),len([sample['mask'] for sample in dataset]))
 
     images_train, images_test, masks_train, masks_test = middle_split(
         [sample['image'] for sample in dataset],
@@ -54,7 +91,41 @@ if __name__ == '__main__':
         train_size
     )
 
+    print(len(images_train),len(images_test),len(masks_train),len(masks_test))
     
+    for path in images_train:
+        shutil.copy(path,os.path.join(dataset_folder, 'train','images'))
+    for path in masks_train:
+        shutil.copy(path,os.path.join(dataset_folder, 'train','masks'))
+    for path in images_test:
+        shutil.copy(path,os.path.join(dataset_folder, 'test','images'))
+    for path in masks_test:
+        shutil.copy(path,os.path.join(dataset_folder, 'test','masks'))
+
+    #train - validation
+    valid_size = valid_size / train_size
+    dataset = ImageNameDataset(
+        os.path.join(dataset_folder, 'train'),
+        sort = True
+    )
+    train_dataset, valid_dataset = random_split(
+        [sample for sample in dataset],
+        [1-valid_size,valid_size],
+        generator=torch.Generator().manual_seed(42)
+    )
+    images_train = [sample['image'] for sample in train_dataset]
+    masks_train = [sample['mask'] for sample in train_dataset]
+    images_valid = [sample['image'] for sample in valid_dataset]
+    masks_valid = [sample['mask'] for sample in valid_dataset]
+
+    print(len(images_train),len(images_valid),len(masks_train),len(masks_valid))
+
+    for path in images_valid:
+        shutil.copy(path,os.path.join(dataset_folder, 'val','images'))
+    for path in masks_valid:
+        shutil.copy(path,os.path.join(dataset_folder, 'val','masks'))
+
+
 
 
 
