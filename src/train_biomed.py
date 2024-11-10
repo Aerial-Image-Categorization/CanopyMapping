48d54fc8cc82cdcbabd3c60939fb008e6f9664a2
mbnczy <banoczy.martin@gmail.com>
2024-11-10 21:41:44 +0000

    add argparse & tweaking params

--- src/train_biomed.py
+++ src/train_biomed.py
@@ -1,24 +1,38 @@
 #import sys
 #sys.path.append('../')
+import argparse
 import os
 import torch
 from models import biomed_UNet as UNet
 from models.biomed_UNet.datasets import ImageDataset
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the model on images and target masks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=50,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=1,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-s', '--img_size', dest='size', type=int, default=512,
+                        help='The size of the images')
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    train_set_path = '../data/2024-09-29-seg-dataset-200/aug_train'
-    valid_set_path = '../data/2024-09-29-seg-dataset-200/val'
-    epochs = 15
-    batch_size = 6
+    args = get_args()
+    img_size = args.size #512
+    train_set_path = f'../data/2024-10-30-loc-dataset-{img_size}/aug_train_u10'
+    valid_set_path = f'../data/2024-10-30-loc-dataset-{img_size}/val'
+    epochs = args.epochs #25
+    batch_size = args.batchsize #6
     lr = 1e-8
     scale = 1
     val = 0.1
     amp = False
     bilinear = False
 
-    model = UNet.model(UNet.config(n_channels=3, n_classes=2, bilinear=bilinear))
+    model = UNet.model(UNet.config(n_channels=3, n_classes=1, bilinear=bilinear))
     loader_args = dict(batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True)
-    #dataset = UNet.ImageDataset(dataset_path)
+    dataset =None#UNet.ImageDataset(dataset_path)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
 
@@ -26,11 +40,12 @@ if __name__ == '__main__':
     valid_set = ImageDataset(valid_set_path)
     
     try:
-        UNet.train_model_Jaccard(
+        UNet.train_net(
             train_set = train_set,
             valid_set = valid_set,
             model=model,
             epochs=epochs,
+            img_size = img_size,
             batch_size=batch_size,
             learning_rate=lr,
             device=device,
--- src/train_centroid.py
+++ src/train_centroid.py
@@ -1,28 +1,51 @@
-import sys
-sys.path.append('../')
+#import sys
+#sys.path.append('../')
+import os
+import argparse
 import torch
 from models import centroid_UNet as UNet
+from models.biomed_UNet.datasets import ImageDataset
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the model on images and target masks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=50,
+                        help='Number of epochs', dest='epochs')
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=1,
+                        help='Batch size', dest='batchsize')
+    parser.add_argument('-s', '--img_size', dest='size', type=int, default=512,
+                        help='The size of the images')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    dataset_path = '../data/2024-04-21-dataset/augmentated/train'
-    epochs = 15
-    batch_size = 6
-    lr = 1e-8
+    args = get_args()
+    img_size = args.size
+    train_set_path = f'../data/2024-10-30-loc-dataset-{img_size}/aug_train_u10'
+    valid_set_path = f'../data/2024-10-30-loc-dataset-{img_size}/val'
+
+    epochs = args.epochs #25
+    batch_size = args.batchsize #6
+    lr = 1e-5
     scale = 1
     val = 0.1
     amp = False
     bilinear = False
     
-    model = UNet.model(n_channels=3, n_classes=2)
-    dataset = UNet.ImageDataset(dataset_path)
+    model = UNet.model(n_channels=3, n_classes=1)
+    dataset = None #UNet.ImageDataset(dataset_path)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
     
+    train_set = ImageDataset(train_set_path)
+    valid_set = ImageDataset(valid_set_path)
+    
     try:
-        UNet.train_model_Jaccard(
-            dataset=dataset,
+        UNet.train_net(
+            train_set = train_set,
+            valid_set = valid_set,
             model=model,
             epochs=epochs,
+            img_size = img_size,
             batch_size=batch_size,
             learning_rate=lr,
             device=device,
@@ -30,14 +53,16 @@ if __name__ == '__main__':
             val_percent=val / 100,
             amp=amp
         )
-    except torch.cuda.OutOfMemoryError:
+    except Exception:#torch.cuda.OutOfMemoryError:
         #logging.error('Detected OutOfMemoryError! Enabling checkpointing to reduce memory usage, but this slows down training. Consider enabling AMP (--amp) for fast and memory efficient training')
         torch.cuda.empty_cache()
         model.use_checkpointing()
-        UNet.train_model(
-            dataset=dataset,
+        UNet.train_net(
+            train_set = train_set,
+            valid_set = valid_set,
             model=model,
             epochs=epochs,
+            img_size = img_size,
             batch_size=batch_size,
             learning_rate=lr,
             device=device,
