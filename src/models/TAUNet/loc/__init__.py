7ac480793b1a7f6bc3e2b84c88e7cf1baa8d5cd5
mbnczy <banoczy.martin@gmail.com>
2024-11-16 03:13:39 +0000

    fixing imports

--- src/models/TAUNet/loc/__init__.py
+++ src/models/TAUNet/loc/__init__.py
@@ -1,3 +1,4 @@
 from .unet_model import model as model
 from .unet_model import config as config
 from .train import train_net as train
+from .train import train_net_loss as train_loss
--- src/models/utils/__init__.py
+++ src/models/utils/__init__.py
@@ -1,3 +1,5 @@
 from .earlystopping import EarlyStopping
 from .evaluation import evaluate
+from .evaluation_transunet import evaluate_transunet
+from .evaluation_seg import evaluate_seg
 from .scores import dice_loss
