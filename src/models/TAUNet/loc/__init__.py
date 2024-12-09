a90f09b4175bb39a981eead674e9f4f1e3d5430e
mbnczy <banoczy.martin@gmail.com>
2024-12-09 22:09:28 +0100

    add imports

--- src/models/TAUNet/loc/__init__.py
+++ src/models/TAUNet/loc/__init__.py
@@ -3,3 +3,4 @@ from .unet_model import config as config
 #from .train import train_net as train
 from .train import train_net_loss as train_loss
 from .train_seg import train_net
+from .test_loc import test_net
