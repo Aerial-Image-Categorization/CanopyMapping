9a7fe9df393b55b4d014bfebeeb6f026ee6d932b
mbnczy <banoczy.martin@gmail.com>
2024-12-09 22:05:49 +0100

    add imports

--- src/models/utils/__init__.py
+++ src/models/utils/__init__.py
@@ -3,3 +3,5 @@ from .evaluation import evaluate
 from .evaluation_transunet import evaluate_transunet
 from .evaluation_seg import evaluate_seg
 from .scores import dice_loss
+from .evaluation_seg_yolo import evaluate_seg_yolo
+from .evaluation_yolo import evaluate_yolo
